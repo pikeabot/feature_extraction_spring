907761b0c8c5dafa7d71ee33cad4acf702fbb2ac
Jocelyn Chang <jrchang@engineer.com>
2016-02-27 02:39:56 -0800

    updated Images Handler which will send image over http

---
+++ feature_extraction_client/src/main/java/edge_detection_app/ImagesHandler.java
@@ -0,0 +1,51 @@
+package edge_detection_app;
+
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.WritableRaster;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+import javax.xml.ws.spi.http.HttpExchange;
+
+import com.sun.net.httpserver.HttpHandler;
+
+
+public class ImagesHandler {
+	
+	public void sendImage(String urlString, int i) throws IOException {
+
+		HttpURLConnection connection=null;
+		ByteArrayOutputStream baos = null;
+		try {
+		    //URL url = new URL("http://www.somewebsite.com");
+			URL url = new URL(urlString);
+		    connection = (HttpURLConnection) url.openConnection();
+		    connection.setDoOutput(true);
+		    connection.setRequestProperty("Content-Type", "image/jpeg");
+		    connection.setRequestMethod("POST");
+		    baos = new ByteArrayOutputStream();
+		    BufferedImage img = ImageIO.read(new File(System.getProperty("user.dir") + "\\tmp\\img" + i + ".jpg"));
+		    baos.write(extractBytes(img)); // your bytes here         
+		    baos.writeTo(connection.getOutputStream());
+		}
+		finally {
+		    if(baos != null)
+		       baos.close();
+		    if(connection != null) 
+		    	connection.disconnect();
+		}  
+	}
+		public byte[] extractBytes (BufferedImage img) throws IOException {
+			 // get DataBufferBytes from Raster
+			 WritableRaster raster = img.getRaster();
+			 DataBufferByte data   = (DataBufferByte) raster.getDataBuffer();
+
+			 return ( data.getData() );
+			}
+	}
