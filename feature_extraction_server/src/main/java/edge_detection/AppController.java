62d53a805ee83e7faba09b08db9dcd8af4e3832d
Jocelyn Chang <jrchang@engineer.com>
2016-02-28 02:00:12 -0800

    updated library

--- feature_extraction_server/src/main/java/edge_detection/AppController.java
+++ feature_extraction_server/src/main/java/edge_detection/AppController.java
@@ -11,9 +11,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.imageio.ImageIO;  
 
